7dee8140a1cb289664be88ae2fc94f0a4b68b84c
Eiji Kitamura <agektmr@google.com>
2026-02-20 16:03:01 +0900

    initial status

--- public/client.js
+++ public/client.js
@@ -40,22 +40,6 @@ export async function _fetch(path, payload = '') {
   }
 };
 
-export const base64url = {
-  encode: function(buffer) {
-    const base64 = window.btoa(String.fromCharCode(...new Uint8Array(buffer)));
-    return base64.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
-  },
-  decode: function(base64url) {
-    const base64 = base64url.replace(/-/g, '+').replace(/_/g, '/');
-    const binStr = window.atob(base64);
-    const bin = new Uint8Array(binStr.length);
-    for (let i = 0; i < binStr.length; i++) {
-      bin[i] = binStr.charCodeAt(i);
-    }
-    return bin.buffer;
-  }
-}
-
 class Loading {
   constructor() {
     this.progress = $('#progress');
@@ -77,15 +61,3 @@ class Loading {
 }
 
 export const loading = new Loading();
-
-// TODO: Add an ability to create a passkey: Create the registerCredential() function.
-
-// TODO: Add an ability to authenticate with a passkey: Create the authenticate() function.
-
-export async function updateCredential(credId, newName) {
-  return _fetch(`/auth/renameKey`, { credId, newName });
-}
-
-export async function unregisterCredential(credId) {
-  return _fetch(`/auth/removeKey?credId=${encodeURIComponent(credId)}`);
-};
--- server.mjs
+++ server.mjs
@@ -36,7 +36,7 @@ app.use(session({
   resave: true,
   saveUninitialized: false,
   proxy: true,
-  cookie:{
+  cookie: {
     httpOnly: true,
     secure: process.env.IS_LOCAL ? false : true,
     sameSite: process.env.IS_LOCAL ? undefined : 'none'
@@ -109,36 +109,6 @@ app.get('/home', (req, res) => {
     project_name: process.env.PROJECT_NAME,
     title: RP_NAME,
   });
-});
-
-app.get('/.well-known/assetlinks.json', (req, res) => {
-  const assetlinks = [];
-  const relation = [
-    'delegate_permission/common.handle_all_urls',
-    'delegate_permission/common.get_login_creds',
-  ];
-  assetlinks.push({
-    relation: relation,
-    target: {
-      namespace: 'web',
-      site: process.env.ORIGIN,
-    },
-  });
-  if (process.env.ANDROID_PACKAGENAME && process.env.ANDROID_SHA256HASH) {
-    const package_names = process.env.ANDROID_PACKAGENAME.split(",").map(name => name.trim());
-    const hashes = process.env.ANDROID_SHA256HASH.split(",").map(hash => hash.trim());
-    for (let i = 0; i < package_names.length; i++) {
-      assetlinks.push({
-        relation: relation,
-        target: {
-          namespace: 'android_app',
-          package_name: package_names[i],
-          sha256_cert_fingerprints: [hashes[i]],
-        },
-      });
-    }
-  }
-  res.json(assetlinks);
 });
 
 app.get('/test', (req, res) => {
